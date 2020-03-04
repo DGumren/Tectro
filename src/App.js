4a096321d0c013d4ee93b1407b6fb78d3c290b3b
NCONTED <NCONTED@mynorth.mymdc.net>
2020-03-04 11:14:33 -0500

    add services and complete routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
+
 import Support from "./views/Support";
 import Login from "./views/Login";
+import Services from "./views/Services";
+
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route path="/support">
-            <Support />
-          </Route>
           <Route path="/login">
             <Login />
+          </Route>
+          <React path="/services">
+            <Services />
+          </React>
+          <Route path="/support">
+            <Support />
           </Route>
           <Route exact path="/">
             <Redirect to="/login" />
