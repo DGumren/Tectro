fe37d5d9388a7385b68cde3010a926c8b2256135
Dcouncildev <dwightcouncil728300@gmail.com>
2020-03-06 14:11:59 -0500

    changes

--- src/views/Support.js
+++ src/views/Support.js
@@ -1,37 +1,66 @@
 import React from "react";
 const Support = () => {
-  return <div>
-  <form>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Customer Contact Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Prelisted Computer Problems</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>(1) Hardware Issues</option>
-      <option>(2) General Computer Repair Issues</option>
-      <option>(3) Memory Issues</option>
-      <option>(4) Virus Issues</option>
-      <option>(5) Heat mangement Issues</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect2">List of Recomended Computer Repair Specialist</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2">
-      <option>(1)Horace Hardware Computer Specialist : 756-364-1485</option>
-      <option>(2)Geiana General Computer Repair Specialist: 334-415-1546</option>
-      <option>(3)Marice Memory Repair Specialist: 456-515-5457</option>
-      <option>(4)Venuski Virus Repair Specialist:145-464-8746</option>
-      <option>(5)Horton Heat mangement Repair Specialist Contact: 454-556-5146</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Description of Problem</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-</form>  
-  </div>;
+  return (
+    <div class="container">
+      <form class="well span8" role="form">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="form-group">
+              <label>First Name</label>
+              <input
+                class="form-control"
+                placeholder="Your First Name"
+                type="text"
+              />
+            </div>
+
+            <div class="form-group">
+              <label>Last Name</label>
+              <input
+                class="form-control"
+                placeholder="Your Last Name"
+                type="text"
+              />
+            </div>
+
+            <div class="form-group">
+              <label>Email Address</label>
+              <input
+                class="form-control"
+                placeholder="Your email address"
+                type="text"
+              />
+            </div>
+
+            <div class="form-group">
+              <label>Subject</label>
+              <select class="form-control" id="subject" name="subject">
+                <option selected value="na">
+                  Choose One:
+                </option>
+
+                <option value="service">General Customer Service</option>
+
+                <option value="suggestions">Suggestions</option>
+
+                <option value="product">Product Support</option>
+              </select>
+            </div>
+          </div>
+
+          <div class="col-md-8">
+            <label>Message</label>
+            <textarea class="form-control" rows="10"></textarea>
+
+            <button class="btn btn-primary pull-right voffset3" type="submit">
+              Send
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Support;
+// until line 50 is done
