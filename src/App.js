8217426d70cdc889756448ed10ea498039e42d2e
Dcouncildev <dwightcouncil728300@gmail.com>
2020-03-09 11:03:39 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
 import Support from "./views/Support";
 import Login from "./views/Login";
 import Services from "./views/Services";
 import Navbar from "./components/Navbar";
-<<<<<<< HEAD
 import "../src/App.css";
-=======
-import Footer from "./components/Footer";
-
->>>>>>> 45728fc98fd475be84bac1544ba864150875182f
 const App = () => {
   return (
     <div>
@@ -22,7 +16,6 @@ const App = () => {
           <Route path="/support" component={Support} />
         </Switch>
       </BrowserRouter>
-      <Footer />
     </div>
   );
 };
--- src/views/Support.js
+++ src/views/Support.js
@@ -63,6 +63,7 @@ const Support = () => {
         </div>
       </form>
     </div>
+    
   );
 };
 
